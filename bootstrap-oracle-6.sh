d0758078f631a80d2b1bd40fa5fd03e3cc064d93
Terry Wang <i@terry.im>
2014-05-20 12:14:00 +1000

    update Ruby to 1.9.3-p547 for Oracle Linux bootstrap scripts

--- bootstrap-oracle-6.sh
+++ bootstrap-oracle-6.sh
@@ -12,7 +12,7 @@
 #
 # --------------------------------------
 ########### Setup Ruby LibYAML Versions #############
-RUBY_VERSION="ruby-1.9.3-p545"
+RUBY_VERSION="ruby-1.9.3-p547"
 RUBY_SOURCE="http://ftp.ruby-lang.org/pub/ruby/1.9/${RUBY_VERSION}.tar.gz"
 LIBYAML_VERSION="yaml-0.1.5"
 LIBYAML_SOURCE="http://pyyaml.org/download/libyaml/${LIBYAML_VERSION}.tar.gz"
